88be32cfa9ddc973454ec25c386c1eb8fa860c62
olaide-hok <olaidekareem43@yahoo.com>
2022-08-28 10:39:32 +0100

    Create Complete Slider Project.
    Alternative.js used in place of App.js.
    
    Create nextSlide and prevSlide functionality to handle onClick event.
    
    Pass nextSlide and prevSlide functionality to next and prev buttons.
    
    Use nextslide function as callback function in useEffect hook.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 
 function App() {
-  const [people, setPeople] = useState(data)
+  const [people] = useState(data)
   const [index, setIndex] = useState(0)
 
   useEffect(() => {
