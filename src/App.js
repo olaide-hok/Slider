181d2ad84fa681c3e6a2f4f366720606382fea2e
olaide-hok <olaidekareem43@yahoo.com>
2022-08-28 09:20:46 +0100

    Setup Conditional CSS class addition.

--- src/App.js
+++ src/App.js
@@ -19,9 +19,17 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const {id, name, title, quote, image} = person
+          let position = 'nextSlide'
+          if (personIndex === index) {
+            position = 'activeSlide'
+          }
+          if (personIndex === index - 1 || 
+            (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide'
+          }
 
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
